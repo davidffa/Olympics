6828d2db919bb391e23baf3841a8efca2f7263e9
David Amorim <davidfilipeamorim@gmail.com>
2022-12-26 23:09:31 +0000

    Searchbar working

--- countries.js
+++ countries.js
@@ -3,29 +3,30 @@ var vm = function () {
   console.log('ViewModel initiated...');
   //---Variáveis locais
   var self = this;
-  self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/countries');
-  self.displayName = 'Olympic Countries List';
+  const baseUri = 'http://192.168.160.58/Olympics/api/countries';
   self.error = ko.observable('');
-  self.passingMessage = ko.observable('');
   self.records = ko.observableArray([]);
+  self.allRecords = ko.observableArray([]);
 
   //--- Page Events
   self.activate = function () {
     console.log('CALL: getCountries...');
-    var composedUri = `${self.baseUri()}?page=1&pagesize=300`
+    var composedUri = `${baseUri}?page=1&pagesize=300`
     ajaxHelper(composedUri, 'GET').done(function (data) {
       console.log(data);
-      hideLoading();
       for (const record of data.Records) {
         if (record.Flag === null) {
           record.Flag = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Flag_with_question_mark.svg/1200px-Flag_with_question_mark.svg.png?20220807030256';
         }
       }
       self.records(data.Records);
-      //self.SetFavourites();
+      self.allRecords(data.Records);
+      hideLoading();
     });
 
-    $("#searchBar").on
+    $("#searchBar").on("input", () => {
+      self.records(self.allRecords().filter(r => r.Name.toLowerCase().startsWith($("#searchBar").val().toLowerCase())));
+    });
   };
 
   //--- Internal functions
