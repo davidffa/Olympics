8da2a020712cbec3e5c319cc67129aa35465cbc7
David Amorim <davidfilipeamorim@gmail.com>
2022-12-27 18:42:45 +0000

    Reformat design

--- games.js
+++ games.js
@@ -2,82 +2,52 @@
 var vm = function () {
   console.log('ViewModel initiated...');
   //---Variáveis locais
+  const BASE_URI = 'http://192.168.160.58/Olympics/api';
+
   var self = this;
-  self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/games');
-  self.displayName = 'Olympic Games editions List';
   self.error = ko.observable('');
-  self.passingMessage = ko.observable('');
   self.records = ko.observableArray([]);
-  self.currentPage = ko.observable(1);
-  self.pagesize = ko.observable(20);
-  self.totalRecords = ko.observable(50);
-  self.hasPrevious = ko.observable(false);
-  self.hasNext = ko.observable(false);
-  self.previousPage = ko.computed(function () {
-    return self.currentPage() * 1 - 1;
-  }, self);
-  self.nextPage = ko.computed(function () {
-    return self.currentPage() * 1 + 1;
-  }, self);
-  self.fromRecord = ko.computed(function () {
-    return self.previousPage() * self.pagesize() + 1;
-  }, self);
-  self.toRecord = ko.computed(function () {
-    return Math.min(self.currentPage() * self.pagesize(), self.totalRecords());
-  }, self);
-  self.totalPages = ko.observable(0);
-  self.pageArray = function () {
-    var list = [];
-    var size = Math.min(self.totalPages(), 9);
-    var step;
-    if (size < 9 || self.currentPage() === 1)
-      step = 0;
-    else if (self.currentPage() >= self.totalPages() - 4)
-      step = self.totalPages() - 9;
-    else
-      step = Math.max(self.currentPage() - 5, 0);
-
-    for (var i = 1; i <= size; i++)
-      list.push(i + step);
-    return list;
-  };
-  self.season = ko.observable();
-
-  //--- Page Events
-  self.activate = function (id, season) {
-    if (["winter", "summer"].includes(season)) {
-      self.season(season);
-    }
-
-    console.log('CALL: getGames...');
-    var composedUri = self.baseUri() + "?page=" + id + "&pageSize=" + self.pagesize();
-    ajaxHelper(composedUri, 'GET').done(function (data) {
-      console.log(data);
-      hideLoading();
-
-      if (self.season() === "winter" || self.season() === "summer") {
-        data.Records = data.Records.filter(r => r.Name.toLowerCase().includes(self.season()));
+  self.allRecords = ko.observableArray([]);
+  // 0 - Winter 1 - Summer 2 - All
+  self.season = ko.observable(2);
+  self.setSeason = function (season) {
+    if (season === 0) {
+      // Winter
+      if (self.season() === 0) {
+        self.records(self.allRecords());
+        self.season(2);
+      } else {
+        self.records(self.allRecords().filter(r => r.Name.includes('Winter')));
+        self.season(0);
+        $("#summer").removeClass("active");
       }
-
-      self.records(data.Records);
-      self.currentPage(data.CurrentPage);
-      self.hasNext(data.HasNext);
-      self.hasPrevious(data.HasPrevious);
-      self.pagesize(data.PageSize)
-      self.totalPages(data.TotalPages);
-      self.totalRecords(data.TotalRecords);
-      //self.SetFavourites();
-    });
-
-    $("#winter").click(() => {
-      self.season("winter")
-    });
-
-    $("#summer").click(() => {
-      self.season("summer")
-    });
+      $("#winter").toggleClass("active");
+    } else if (season === 1) {
+      // Summer
+      if (self.season() === 1) {
+        self.records(self.allRecords());
+        self.season(2);
+      } else {
+        self.records(self.allRecords().filter(r => r.Name.includes('Summer')));
+        self.season(1);
+        $("#winter").removeClass("active");
+      }
+      $("#summer").toggleClass("active");
+    }
   }
 
+  //--- Page Events
+  self.activate = function () {
+    console.log('CALL: getGames...');
+    var composedUri = `${BASE_URI}/games?page=1&pagesize=100`
+    ajaxHelper(composedUri, 'GET').done(function (data) {
+      console.log(data);
+
+      self.allRecords(data.Records);
+      self.records(data.Records);
+      hideLoading();
+    });
+  }
 
   //--- Internal functions
   function ajaxHelper(uri, method, data) {
@@ -96,11 +66,6 @@ var vm = function () {
     });
   }
 
-  function sleep(milliseconds) {
-    const start = Date.now();
-    while (Date.now() - start < milliseconds);
-  }
-
   function showLoading() {
     $("#myModal").modal('show', {
       backdrop: 'static',
@@ -113,31 +78,9 @@ var vm = function () {
     })
   }
 
-  function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
-    console.log("sPageURL=", sPageURL);
-    for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
-
-      if (sParameterName[0] === sParam) {
-        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-      }
-    }
-  };
-
   //--- start ....
   showLoading();
-  var pg = getUrlParameter('page');
-  var season = getUrlParameter('season');
-  console.log(pg);
-  if (pg == undefined)
-    self.activate(1, season);
-  else {
-    self.activate(pg, season);
-  }
+  self.activate();
   console.log("VM initialized!");
 };
 
