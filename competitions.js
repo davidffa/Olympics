16c57563073a5229794ae84156eca77e43313c7b
David Amorim <davidfilipeamorim@gmail.com>
2022-12-27 14:49:13 +0000

    List competitions

--- competitions.js
+++ competitions.js
@@ -1,13 +1,64 @@
 var vm = function () {
+  const baseUri = 'http://192.168.160.58/Olympics/api';
+
   var self = this;
 
   self.error = ko.observable('');
+  self.records = ko.observableArray([]);
+  self.currentPage = ko.observable(1);
+  self.pagesize = ko.observable(20);
+  self.totalRecords = ko.observable(50);
+  self.hasPrevious = ko.observable(false);
+  self.hasNext = ko.observable(false);
+  self.previousPage = ko.computed(function () {
+    return self.currentPage() * 1 - 1;
+  }, self);
+  self.nextPage = ko.computed(function () {
+    return self.currentPage() * 1 + 1;
+  }, self);
+  self.fromRecord = ko.computed(function () {
+    return self.previousPage() * self.pagesize() + 1;
+  }, self);
+  self.toRecord = ko.computed(function () {
+    return Math.min(self.currentPage() * self.pagesize(), self.totalRecords());
+  }, self);
+  self.totalPages = ko.observable(0);
+  self.pageArray = function () {
+    var list = [];
+    var size = Math.min(self.totalPages(), 5);
+    var step;
+    if (size < 5 || self.currentPage() === 1)
+      step = 0;
+    else if (self.currentPage() >= self.totalPages() - 2)
+      step = self.totalPages() - 5;
+    else
+      step = Math.max(self.currentPage() - 3, 0);
 
-  const baseUri = 'http://192.168.160.58/Olympics/api';
+    for (var i = 1; i <= size; i++)
+      list.push(i + step);
+    return list;
+  };
+  self.loading = ko.observable(true);
+  self.selectCompetition = function (competition) {
+    // TODO: implement this function
+    console.log("Selected competition: ", competition);
+  }
 
   // Page events
-  self.activate = function () {
-
+  self.activate = function (id) {
+    console.log('CALL: getCompetitions...');
+    var composedUri = `${baseUri}/competitions?page=${id}&pagesize=${self.pagesize()}`;
+    ajaxHelper(composedUri, 'GET').done(function (data) {
+      console.log(data);
+      hideLoading();
+      self.records(data.Records);
+      self.currentPage(data.CurrentPage);
+      self.hasNext(data.HasNext);
+      self.hasPrevious(data.HasPrevious);
+      self.pagesize(data.PageSize)
+      self.totalPages(data.TotalPages);
+      self.totalRecords(data.TotalRecords);
+    });
   }
 
   //--- Internal functions
@@ -32,11 +83,13 @@ var vm = function () {
       backdrop: 'static',
       keyboard: false
     });
+    self.loading(true);
   }
   function hideLoading() {
     $('#myModal').on('shown.bs.modal', function (e) {
       $("#myModal").modal('hide');
-    })
+    });
+    self.loading(false);
   }
 
   function getUrlParameter(sParam) {
@@ -55,8 +108,11 @@ var vm = function () {
   };
 
   //--- start ....
-  // showLoading();
-  self.activate();
+  showLoading();
+  var id = getUrlParameter('page');
+  if (id === undefined)
+    id = 1;
+  self.activate(id);
   console.log("VM initialized!");
 }
 
