e6e68ee4d1f1dafd576119c940eb40c71119588c
GCapaross <119590128+GCapaross@users.noreply.github.com>
2022-12-29 21:13:36 +0000

    EZ

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ $('document').ready(() => {
     new bootstrap.Carousel('#mainCarousel', {
         interval: 10000
     })
+
+
+
+    let on = true;
+setInterval(() => {
+  if (on) {
+    $(".luzes").attr("src","./assets/Redout.png")
+  } else {
+    $(".luzes").attr("src","./assets/Green out.png")
+  }
+  on = !on;
+}, 500);
+
 });
+
